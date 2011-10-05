339b0d01a0ba5a696ce5404f1e58534d0cd48005
Stals <superstals@gmail.com>
2011-10-05 11:49:23 +0400

    rewrote delete single comment and added function to delete multi line comments

--- main.py
+++ main.py
@@ -19,67 +19,76 @@ languages = {"-cpp"    : [ '//', ('/*', '*/') ],
              "-delphi" : [ '//', ('{', '}') ],
              "-python" : [ '#',  ('"""', '"""') ],
              "-ruby"   : [ '#',  ('=begin', '=end') ]
-             }
+}
 
 flags = ["-auto", # gives a key depending on file extension
          "-all"]  # deletes comments for all the languages
 
 #TODO  поддержка нескольких сингл-лайн комментариев для одного языка
-def deleteSingleLineComments(lines,singleComment):#TODO do not delete line separators
-    newLines = []
-    for line in lines:
-        if singleComment in line:
-            newLine = ""
-            commentIndex = line.find(singleComment)
-            newLine = line[:commentIndex] + "\n" #TODO add /n only if /n is at the end of the line
-            newLines.append(newLine)
-        else:
-            newLines.append(line)
+def deleteSingleLineComments(text, singleComment):#TODO Rewrite coz this is veeeery bad solutuion for big files ( calles copy for the whole text for each comment )
 
-    print (newLines)
-    return newLines
+	while singleComment in text :
+		commentBeginIndex = text.find(singleComment)
+		commentEndIndex = text.find('\n',commentBeginIndex)
+		# copy  everything except commented text
+		text = text[:commentBeginIndex]+text[commentEndIndex:]
+
+	return text
+
+#TODO  поддержка нескольких мульти-лайн комментариев для одного языка
+def deleteMultiLineComments(text, comment):
+
+	while (comment[0] in text) and (comment[1] in text) :
+		commentBeginIndex = text.find(comment[0])
+		commentEndIndex = text.find(comment[1], commentBeginIndex + len(comment[1]))
+		# copy  everything except commented text
+		text = text[:commentBeginIndex] + \
+		       text[commentEndIndex + len(comment[1]):]
+
+
+	return text
 
 def main(): #TODO make separate functions for better understanding
-    comments = [] #stores comments to delete
-    files = [] #stores files that need to be cleaned
-    if len(sys.argv) > 1:
-        # Get comments and files
-        if sys.argv[1][0] == '-':
-            # If the first argument is a key (-cpp,-java,ect)
-            files = sys.argv[2:]
-            if sys.argv[1] in languages.keys():
-                comments = languages[sys.argv[1]]
-            else:
-                print("Wrong Key\nKnown keys:")
-                for language in languages:
-                    print (language)
-                exit(0)
-        else:
-            files = sys.argv[1:]
-            # Let user input comments in a console
-            comments = [ input("Input single line comment:"),
-                       ( input("Input begin of multi-line comment:"),
-                         input("Input end if multi-line comment:") )]
-        # Now we have "comments" to delete from "files"
-        for filename in files:
+	comments = [] #stores comments to delete
+	files = [] #stores files that need to be cleaned
+	if len(sys.argv) > 1:
+		# Get comments and files
+		if sys.argv[1][0] == '-':
+			# If the first argument is a key (-cpp,-java,ect)
+			files = sys.argv[2:]
+			if sys.argv[1] in languages.keys():
+				comments = languages[sys.argv[1]]
+			else:
+				print("Wrong Key\nKnown keys:")
+				for language in languages:
+					print (language)
+				exit(0)
+		else:
+			files = sys.argv[1:]
+			# Let user input comments in a console
+			comments = [ input("Input single line comment:"),
+					( input("Input begin of multi-line comment:"),
+					  input("Input end if multi-line comment:") )]
+			# Now we have "comments" to delete from "files"
+		for filename in files:
 
-            f = open(filename, 'r')
-            lines = f.readlines()
-            f.close()
+			f = open(filename, 'r')
+			text = f.read()
+			f.close()
 
-            newLines = deleteSingleLineComments(lines, comments[0])
-            #newLines = deleteMultiLineComments(newLines,comments[1])
+			newText = deleteSingleLineComments(text, comments[0])
+			newText = deleteMultiLineComments(newText,comments[1])
 
-            if not os.path.isdir("withoutComments"):
-                os.mkdir("withoutComments")
-            f = open("withoutComments\\"+filename, 'w')
-            f.writelines(newLines)
-            f.close()
+			if not os.path.isdir("withoutComments"):
+				os.mkdir("withoutComments")
+			f = open("withoutComments\\" + filename, 'w')
+			f.write(newText)
+			f.close()
 
-    else:
-        #if no arguments were passed
-        print("Expected: commentsDeleter.py -language 'saveToDir' 'file 1' 'file 2' 'ect...'")
+	else:
+		#if no arguments were passed
+		print("Expected: commentsDeleter.py -language 'saveToDir' 'file 1' 'file 2' 'ect...'")
 
 
 if __name__ == '__main__':
-    main()
+	main()
