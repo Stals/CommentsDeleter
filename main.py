53bb259bb1721c4818266298ab269bd6d89c3bc4
Stals <superstals@gmail.com>
2011-10-24 14:16:49 +0400

    rewrote languages as a class , finished writing the main function and implemented number of functions. Also deleted a lot of TODO's

--- main.py
+++ main.py
@@ -1,30 +1,33 @@
 # Delets every char that follows a comment in that line
 # argv's should look like this:
-# commentsDeleter.py -cpp "saveToDir" "file 1" "file 2" "ect..."
+# commentsDeleter.py -cpp "file 1" "file 2" "ect..."
 #TODO - auto.
 #TODO - all Когда сделаю поддержку нескольких вариантов комментариев для мультилйна и синглайна  - когда выбирают -all , нужно создать comments сложа все комментарии для других языков
-
-
-#TODO Удаление должно происходить по тексту, а не по строкам, и нужно удалять всё что после комментария но до переноса на новую строку
-# Это касается как сингл- так и мулти- лайн комментариев
-# Пока есть идея только посимвольно копировать в строку, пока нет комментария, потом пропускать символы, до тех пор пока не будет \n
-# А в мултилайн  -  до тех пор пока не найдётся закрывающий комментарий
-# И на выходе функция выдаёт текст 
+#TODO подумать о переназвании comments
 
 import sys
 import os
-#TODO подумать о переназвании languages и comments
 
-#TODO also add file extension for each language
-languages = {"-cpp"    : [ '//', ('/*', '*/') ],
-             "-java"   : [ '//', ('/*', '*/') ],
-             "-delphi" : [ '//', ('{', '}') ],
-             "-python" : [ '#',  ('"""', '"""') ],
-             "-ruby"   : [ '#',  ('=begin', '=end') ]
-}
+class Language:
+	def __init__(self,name,flag,singleLineComment,multiLineComment,fileExtensions):
+		self.name = name
+		self.flag = flag
+		self.singleLineComment = singleLineComment
+		self.multiLineComment = multiLineComment
+		self.fileExtensions = fileExtensions
+
+
+languages = []
+languages.append( Language( 'C++',    '-cpp',    '//', ('/*', '*/'),       ('.cpp', '.h', '.hpp') ) )
+languages.append( Language( 'JAVA',   '-java',   '//', ('/*', '*/'),       ('') ) )
+languages.append( Language( 'Delphi', '-delphi', '//', ('{', '}'),         ('') ) )
+languages.append( Language( 'Python', '-python', '#',  ('"""', '"""'),     ('.py') ) )
+languages.append( Language( 'Ruby',   '-ruby',   '#',  ('=begin', '=end'), ('.rb') ) )
+
 
 flags = ["-auto", # gives a key depending on file extension
          "-all"]  # deletes comments for all the languages
+
 
 #TODO  поддержка нескольких сингл-лайн комментариев для одного языка
 def deleteSingleLineComments(text, singleComment):#TODO Rewrite coz this is veeeery bad solutuion for big files ( calles copy for the whole text for each comment )
@@ -36,16 +39,22 @@ def deleteSingleLineComments(text, singleComment):#TODO Rewrite coz this is veee
 		text = text[:commentBeginIndex] + text[commentEndIndex:]
 	return text
 
+
 #TODO  поддержка нескольких мульти-лайн комментариев для одного языка
 def deleteMultiLineComments(text, comment):
-	#
 	while (comment[0] in text) and (comment[1] in text) :
 		commentBeginIndex = text.find(comment[0])
-		commentEndIndex = text.find(comment[1], commentBeginIndex + len(comment[1]))
+		commentEndIndex = text.find(comment[1], commentBeginIndex + len(comment[1])) # TODO mb len(comment[0]) needed?
 		# copy  everything except commented text
 		text = text[:commentBeginIndex] + \
 		       text[commentEndIndex + len(comment[1]):]
 	return text
+
+
+def deleteComments(text, comments):
+	newText = deleteMultiLineComments(text , comments[1])
+	newText = deleteSingleLineComments(newText, comments[0])
+	return newText
 
 
 def getTextFromFile(filename):
@@ -65,91 +74,51 @@ def saveTextToFile(dirName, filename, text):
 
 
 def getCommentsFromUser():
+	print("No flag was specified")
 	singleLineComment = input("Input single line comment:")
 	multiLineCommentBegin = input("Input begin of multi-line comment:")
 	multiLineCommentEnd = input("Input end if multi-line comment:")
 	return [singleLineComment, (multiLineCommentBegin, multiLineCommentEnd)]
 
 
-def main(): #TODO Нужно переписать, а то вообще не возможно уследить за логикой,
-			#TODO нужно сдлеть отдельную функцию которая будет колечать комментс ( в том числе вызывать функцию getCommentsFromUser())
-			#ТОесть в неё передаётся флаг - а она возвращает те комментарии которые соответствуют этому флагу
+def getComments(flag):
+	if flag == "":
+		return getCommentsFromUser()
+	else:
+		for language in languages:
+			if flag == language.flag:
+				return [language.singleLineComment, (language.multiLineComment[0], language.multiLineComment[1])]
 
-	"""
+		#if flag is not known
+		print("Wrong flag\nKnown flags:")
+		for language in languages:
+			print ("{0} for {1}".format(language.flag, language.name))
+		exit(0)
+
+
+def main():
 	files = [] # stores files that need to be cleaned
 	flag = "" # stores flag passed as an argument
 	if len(sys.argv) > 1:
-		if (sys.argv[1][0] == '-') && (( len(sys.argv) > 2):
+		if (sys.argv[1][0] == '-') and (( len(sys.argv) > 2)):
 			#if first argument is a flag AND There are files after the flag
-			flag = argv[1]
-			files = argv[2:]
+			flag = sys.argv[1]
+			files = sys.argv[2:]
 		else:
+			#flag was not specified
 			flag = ""
-			files = argv[1:]
+			files = sys.argv[1:]
 	else:
 		# if no arguments were passed
-		print("Expected: commentsDeleter.py -language 'saveToDir' 'file 1' 'file 2' 'ect...'")
+		print("Expected: commentsDeleter.py -language 'file 1' 'file 2' 'ect...'")
+		exit(0)
 
-	comments = getComments(flag) #TODO Should ask user to give comments if flag == ""
+	comments = getComments(flag)
 	for filename in files:
 		text = getTextFromFile(filename)
 		newText = deleteComments(text, comments)
 		dirName = "withoutComments"
 		saveTextToFile(dirName, filename, newText)
-	"""
-
-
-
-	comments = [] # stores comments to delete
-	files = [] # stores files that need to be cleaned
-	flag = "" # stores flag passed as an argument
-	if len(sys.argv) > 1:
-		# Get comments and files
-		if sys.argv[1][0] == '-':
-			flag = sys.argv[1]
-			# If the first argument is a key (-cpp,-java,ect)
-			files = sys.argv[2:]
-			if flag in languages.keys():
-				comments = languages[sys.argv[1]]
-
-			elif flag in flags:#TODO
-				pass
-			else:
-				print("Wrong Key\nKnown keys:")
-				for language in languages:
-					print (language)
-				exit(0)
-		else:
-			files = sys.argv[1:]
-			# Let user input comments in a console
-			comments = getCommentsFromUser()
-
-########################################################################################################################
-		for filename in files:
-			#Get text from file , delete comments and save this text back to file
-			text = getTextFromFile(filename)
-
-			newText = "" # stores text without comments
-			if flag == "-all":
-				newText = text
-				for languageComments in languages.values():
-					newText = deleteSingleLineComments(newText, languageComments[0])
-					newText = deleteMultiLineComments(newText, languageComments[1])
-			elif flag == "-auto":
-				pass
-			else:
-				# if language or no flag was selected
-				newText = deleteSingleLineComments(text, comments[0])
-				newText = deleteMultiLineComments(newText, comments[1])
-
-			print( newText )
-			dirName = "withoutComments"
-			saveTextToFile(dirName, filename, newText)
-
-	else:
-		# if no arguments were passed
-		print("Expected: commentsDeleter.py -language 'saveToDir' 'file 1' 'file 2' 'ect...'")
-
 
 if __name__ == '__main__':
 	main()
